945fca107c44fb18c4f383b4b143f0c83e92c240
diodemusic <diodemusicuk@gmail.com>
2023-02-11 08:31:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # :3
 import discord
 import secret
+from datetime import datetime
 
 intents = discord.Intents(messages=True)
 intents.message_content = True
@@ -10,15 +11,19 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"<{client.user}> online.")
+    print(f"{client.user} bot online.")
 
 @client.event
 async def on_message(message):
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    
+    print(f"{dt_string} <{message.author}> {message.content}")
+    
     if message.author == client.user:
         return
 
     if message.content.startswith(".hello") and message.content == ".hello":
-        print(f"<{message.author}> {message.content}")
         await message.channel.send("hello :)")
 
 client.run(secret.discordAPIKey)
